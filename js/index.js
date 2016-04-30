fd83cbb274e348f7f19809bc4554230c84f1566f
SUMAN GUHA <sgmagicaccount@gmail.com>
2016-04-30 22:35:04 +0530

    ver3

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('li.feilds').filter(':nth-child(n+4)').addClass('hide');
-    $('ul').on('click','li.title',function(){
+    $('ul').on('click', 'li.title', function () {
         $(this).next().slideDown(200).siblings('li.feilds').slideUp(200);
-    })
+    });
 });
+function validateform() {
+    for (var i = 0; i < myform.elements.length; i++) {
+        if (myform.elements[i].className == "req" && myform.elements[i].value.length == 0) {
+            alert('Fill all required Fields');
+            return false;
+        }
+    }
+}
+
