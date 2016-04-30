cd55bf0b9dfbeb260898094d56b9549d8f551316
SUMAN GUHA <sgmagicaccount@gmail.com>
2016-04-30 21:56:49 +0530

    ver2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    
+    $('li.feilds').filter(':nth-child(n+4)').addClass('hide');
+    $('ul').on('click','li.title',function(){
+        $(this).next().slideDown(200).siblings('li.feilds').slideUp(200);
+    })
 });
